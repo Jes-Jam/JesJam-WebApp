477aefe09ef6bcb5dab01da55ef24fa61dea3553
Pun VireakRoth <vireakrothpun@gmail.com>
2024-10-28 10:25:26 +0700

    UI: Footer for the landing page. 🎨

--- app/(landing_page)/footer.tsx
+++ app/(landing_page)/footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
-        <footer>
-            <p>Footer</p>
+        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+            <div className="max-w-screen-xl mx-auto flex items-center justify-between h-full">
+                <p className="text-slate-600">
+                    © 2024 JesJam. All rights reserved.
+                </p>
+            </div>
         </footer>
     );
 }
