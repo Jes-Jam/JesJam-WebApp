53a2638d38460dab49b7d897c1b22fd7d6697b81
Lay <senglaypann@gmail.com>
2024-12-01 11:48:53 +0700

    feat: change landing page layout

--- app/(landing_page)/layout.tsx
+++ app/(landing_page)/layout.tsx
@@ -9,7 +9,7 @@ const LandingPageLayout = ({ children }: Props) => {
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
-            <main className="flex-1 flex flex-col items-center justify-center">
+            <main className="flex-1 flex flex-col max-w-[1280px] mx-auto items-center">
                 {children}
             </main>
             <Footer />
--- app/(landing_page)/page.tsx
+++ app/(landing_page)/page.tsx
@@ -1,41 +1,71 @@
 import Image from "next/image";
-import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignUpButton, SignInButton } from "@clerk/nextjs"
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignUpButton,
+  SignInButton,
+} from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
+import { div } from "framer-motion/client";
 
 export default function Home() {
-    return (
-        <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-x-8">
-            <div className="relative w-[240px] h-[240px] lg:w-[780px] lg:h-[780px] mb:8 lg:mb:0">
-                <Image src="/images/landing_page_animation.svg" alt="jesjam-logo" fill className="object-contain" />
-            </div>
-            <div className="flex flex-col gap-y-9">
-                <h1 className="text-3xl lg:text-4xl font-bold text-sky-600 text-left whitespace-nowrap">
-                    Learn with ease! &nbsp;😁
-                </h1>
-                <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
-                    <ClerkLoading>
-                        <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-                    </ClerkLoading>
-                    <ClerkLoaded>
-                        <SignedOut>
-                            <SignUpButton {...({ afterSignUpUrl: "/study", afterSignInUrl: "/study" } as any)} mode="modal">
-                                <Button size="lg" variant="primary" className="w-full">Get Started</Button>
-                            </SignUpButton>
-                            <SignInButton {...({ afterSignUpUrl: "/study", afterSignInUrl: "/study" } as any)} mode="modal">
-                                <Button size="lg" variant="primaryOutline" className="w-full">I already have an account</Button>
-                            </SignInButton>
-                        </SignedOut>
-                        <SignedIn>
-                            <Button size="lg" variant="primary" className="w-full" asChild>
-                                <Link href="/study">Continue Learning</Link>
-                            </Button>
-                        </SignedIn>
-                    </ClerkLoaded>
-                </div>
-            </div>
+  return (
+    <div className="flex items-center">
+      <div className="flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-x-8 mt-[100px]">
+        <div className="relative w-[240px] h-[240px] lg:w-[565px] lg:h-[480px] mb:8 lg:mb:0 ">
+          <Image
+            src="/images/landing_page_animation.svg"
+            alt="jesjam-logo"
+            fill
+            className="object-contain"
+          />
         </div>
-    );
+        <div className="flex flex-col gap-y-9">
+          <h1 className="text-3xl lg:text-4xl font-bold text-sky-600 text-left whitespace-nowrap">
+            Learn with ease! &nbsp;😁
+          </h1>
+          <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+            <ClerkLoading>
+              <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+            </ClerkLoading>
+            <ClerkLoaded>
+              <SignedOut>
+                <SignUpButton
+                  {...({
+                    afterSignUpUrl: "/study",
+                    afterSignInUrl: "/study",
+                  } as any)}
+                  mode="modal"
+                >
+                  <Button size="lg" variant="primary" className="w-full">
+                    Get Started
+                  </Button>
+                </SignUpButton>
+                <SignInButton
+                  {...({
+                    afterSignUpUrl: "/study",
+                    afterSignInUrl: "/study",
+                  } as any)}
+                  mode="modal"
+                >
+                  <Button size="lg" variant="primaryOutline" className="w-full">
+                    I already have an account
+                  </Button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <Button size="lg" variant="primary" className="w-full" asChild>
+                  <Link href="/study">Continue Learning</Link>
+                </Button>
+              </SignedIn>
+            </ClerkLoaded>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
