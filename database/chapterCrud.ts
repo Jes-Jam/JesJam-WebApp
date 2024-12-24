eac7481babdc32265f5053d82fd147735c81233c
Lay <senglaypann@gmail.com>
2024-12-24 23:22:11 +0700

    added create chapter controller

--- database/chapterCrud.ts
+++ database/chapterCrud.ts
@@ -1,22 +1,38 @@
+"use server"
+
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { chapters } from "./schema";
+import { chapters, classes } from "./schema";
 import { eq, and } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { cache } from "react";
 
-// export const createChapter = cache(async (classId: number, title: string) => {
-//   const { userId } = await auth();
+export const createChapters = cache(async (classId: number, newChapter: Array<{ title: string, description: string }>) => {
+  const { userId } = await auth();
 
-//   if (!userId) {
-//     return;
-//   }
+  if (!userId) throw new Error("unauthorized");
 
-//   await db.insert(chapters).values({
-//     title,
-//     classId,
-//     ownerId: userId
-//   });
+  const mutatingClass = await db.query.classes.findFirst({
+    where: eq(classes.id, classId)
+  });
 
-//   revalidatePath(`/classes/${classId}`);
-// });
+  if (!mutatingClass) throw new Error("Class not found");
+
+  const ownedMutatingClass = await db.query.classes.findFirst({
+    where: and(eq(classes.id, classId), eq(classes.ownerId, userId))
+  });
+
+  if (!ownedMutatingClass) throw new Error("Unauthorized");
+
+  await Promise.all(newChapter.map(async (chapter, index) => {
+    await db.insert(chapters).values({
+      classId,
+      title: chapter.title,
+      description: chapter.description,
+      order: index + 1
+    })
+  }));
+
+  revalidatePath(`/classes/${classId}/chapters`);
+});
+
