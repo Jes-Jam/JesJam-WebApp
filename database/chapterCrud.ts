deeb2688b8b26c68b772dbd43501e36b65ac600b
Lay <senglaypann@gmail.com>
2024-12-25 00:43:14 +0700

    added update chapter controller

--- database/chapterCrud.ts
+++ database/chapterCrud.ts
@@ -3,9 +3,10 @@
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { chapters, classes } from "./schema";
-import { eq, and } from "drizzle-orm";
+import { eq, and, inArray } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { cache } from "react";
+import { a } from "framer-motion/client";
 
 export const createChapters = cache(async (classId: number, newChapter: Array<{ title: string, description: string }>) => {
   const { userId } = await auth();
@@ -36,3 +37,63 @@ export const createChapters = cache(async (classId: number, newChapter: Array<{ 
   revalidatePath(`/classes/${classId}/chapters`);
 });
 
+
+export const updateChapters = cache(async (classId: number, updatedChapters: Array<{ id?: number, title: string, description: string }>) => {
+  const { userId } = await auth();
+
+  if (!userId) throw new Error("unauthorized");
+
+  const mutatingClass = await db.query.classes.findFirst({
+    where: eq(classes.id, classId)
+  });
+
+  if (!mutatingClass) throw new Error("Class not found");
+
+  const ownedMutatingClass = await db.query.classes.findFirst({
+    where: and(eq(classes.id, classId), eq(classes.ownerId, userId))  
+  });
+
+  if (!ownedMutatingClass) throw new Error("Unauthorized");
+
+  const existingChapters = await db.query.chapters.findMany({
+    where: eq(chapters.classId, classId)
+  });
+
+  const existingChaptersIds = existingChapters
+    .map(chapters => chapters.id);
+
+  const incomingChaptersIds = updatedChapters
+    .filter(chapter => chapter.id !== undefined)
+    .map(chapter => chapter.id);
+
+  const chapterIdsToDelete = existingChaptersIds
+    .filter(id => !incomingChaptersIds.includes(id))
+
+  if (chapterIdsToDelete.length > 0) {
+    await db.delete(chapters).where(
+      and(eq(chapters.classId, classId), inArray(chapters.id, chapterIdsToDelete))
+    )
+  }
+
+  await Promise.all(updatedChapters.map(async (chapter, index) => {
+    if (chapter.id ) {
+      await db.update(chapters).set({
+        title: chapter.title,
+        description: chapter.description,
+        order: index + 1
+      })
+      .where(and(eq(chapters.id, chapter.id), eq(chapters.classId, classId)))
+    }
+
+    if (!chapter.id) {
+      await db.insert(chapters).values({
+        classId,
+        title: chapter.title,
+        description: chapter.description,
+        order: index + 1
+      })
+    }
+  }));
+
+  revalidatePath(`/classes/${classId}/chapters`);
+});
