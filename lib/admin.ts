b686afec1987ea13515ae40aa91940abc03eab8a
Pun VireakRoth <vireakrothpun@gmail.com>
2025-01-07 17:54:44 +0700

    Added second admin.

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,7 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 
 const allowedUsers = [
-    process.env.ADMIN_USER_ID
+    process.env.ADMIN_USER_ID,
+    process.env.ADMIN_USER_ID_2
 ];
 
 export const getIsAdmin = async () => {
